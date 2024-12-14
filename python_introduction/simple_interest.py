6775c4cff7fb363b470c4f9adbf0cf1a0142fbb5
musapeace <mubarakinda@gmail.com>
2024-12-14 10:50:40 +0000

    calculating simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+# a complete python script for calculating simple interest on an investment over a period of time.
+
+# defining variables 
+principal = 1000
+rate = 0.05
+time = 3
+
+# calculating simple interest I 
+interest = principal*rate*time
+print(interest)
+
