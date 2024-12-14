192e170099545f288117e8127f329cfb5ac92e33
musapeace <mubarakinda@gmail.com>
2024-12-14 08:58:55 +0000

    basic python operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+# declaring variables number1 and number2
+number1 = 10
+number2 = 5
+
+# performing addition operation
+sum = number1 + number2
+
+# performing subtraction operation
+subtr = number1 - number2
+
+# performing multiplication operation
+mult = number1*number2
+
+print(sum)
+print(subtr)
+print(mult)
