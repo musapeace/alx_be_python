d4395e0688ac0a98357442efc34846f13675d6b3
musapeace <mubarakinda@gmail.com>
2025-01-24 00:25:56 +0000

    Unit Tests for a Simple Calculator Class

---
+++ programming_paradigm/simple_calculator.py
@@ -0,0 +1,22 @@
+class SimpleCalculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        
+    def add(self):
+        return self.a + self.b
+    
+    def subtract(self):
+        return self.a - self.b
+    
+    def multiply(self):
+        return self.a * self.b
+    
+    def divide(self):
+        if self.b == 0:
+            return None
+        return self.a / self.b
+    
+
+
+                
