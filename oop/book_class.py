8e1fae7c27026fb20fbe0d1cd551359a39d48ddb
musapeace <mubarakinda@gmail.com>
2025-01-26 01:31:53 +0000

    exercise on python magic method

---
+++ oop/book_class.py
@@ -0,0 +1,18 @@
+class Book:
+    def __init__ (self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+
+    def __repr__(self):
+      return  f"Book('{self.title}', '{self.author}', {self.year})"
+
+
+    def __str__(self):
+       return f"('{self.title}', by {self.author}, pblished in {self.year} )"
+    
+
+    def __del__ (self):
+       print(f"Book '{self.title}' is being deleted.")
+            
---
+++ oop/main.py
@@ -0,0 +1,17 @@
+from book_class import Book
+
+def main():
+    my_book = Book("1984", "George Orwell", 1949)
+
+# str method illustration 
+    print(my_book)
+
+
+# repr method print example 
+    print(repr(my_book))
+
+# deleting a book instance 
+    del my_book
+
+if __name__ == "__main__":
+    main()
